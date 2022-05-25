6560ba70572018dacb41f1407591fcee1edad5dd
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-05-25 11:33:21 -0700

    Add edit functionality

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chats");
+    this.unsub;
   }
   async addChat(message) {
     // Format Chat Object
@@ -22,20 +23,43 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+
   getChats(callback) {
-    this.chats.onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === "added") {
-          // Update The UI
-          callback(change.doc.data());
-        }
+    this.unsub = this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            // Update The UI
+            callback(change.doc.data());
+          }
+        });
       });
-    });
+  }
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
   }
 }
 
-const chatroom = new Chatroom("gaming", "Ryan");
+const chatroom = new Chatroom("general", "Ryan");
 
 chatroom.getChats((data) => {
   console.log(data);
 });
+
+setTimeout(() => {
+  chatroom.updateRoom("gaming");
+  chatroom.updateName("yoshi");
+  chatroom.getChats((data) => {
+    console.log(data);
+  });
+  chatroom.addChat("hello");
+}, 3000);
